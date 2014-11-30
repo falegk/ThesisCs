952594b5868fe381578b69e72dae538621b0a18c
falegk <falegk1@gmail.com>
2014-11-30 17:11:06 +0200

    ThesisCs Theme v1.0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,47 @@
 class ApplicationController < ActionController::Base
+  #before_action :authenticate_user!
+  before_action :check_users_type
+
+  layout "thesisCs/thesisCs"
+  rescue_from DeviseLdapAuthenticatable::LdapException do |exception|
+    render :text => exception, :status => 500
+  end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  #before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  #checks if the user has chosen department
+  def department_empty?
+     User.find(current_user) do |p|
+      if p.student.department == nil
+        redirect_to profile_path
+      else
+        @ok = 'ok'
+      end
+     end
+
+  end
+
+
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
+  end
+
+  #check user if a student or teacher
+  def check_users_type
+    @user = User.all.find(current_user) do |p|
+      if p.student == nil
+          p.create_student
+          redirect_to profile_path
+      else
+        @ok = "This user ( #{p.email} ) is a student"
+      end
+    end
+  end
+
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,18 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w( bootstrap.css )
+Rails.application.config.assets.precompile += %w( plugins/metisMenu/metisMenu.min.css )
+Rails.application.config.assets.precompile += %w( sb-admin-2.css )
+Rails.application.config.assets.precompile += %w( font-awesome-4.1.0/css/font-awesome.min.css )
+#Rails.application.config.assets.precompile += %w( bootstrap.css )
+
+Rails.application.config.assets.precompile += %w( jquery-1.11.0.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( plugins/metisMenu/metisMenu.min.js )
+Rails.application.config.assets.precompile += %w( sb-admin-2.js )
