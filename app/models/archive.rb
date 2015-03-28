55977670862365f95e2c48cd6aef5cff82d3220a
falegk <falegk1@gmail.com>
2015-03-28 14:12:22 +0200

    delete archive migration & model

--- app/models/archive.rb
+++
@@ -1,2 +0,0 @@
-class Archive < ActiveRecord::Base
-end
--- db/migrate/20150208095801_create_archives.rb
+++
@@ -1,8 +0,0 @@
-class CreateArchives < ActiveRecord::Migration
-  def change
-    create_table :archives do |t|
-
-      t.timestamps
-    end
-  end
-end
